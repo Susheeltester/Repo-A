976d2086eaeab6372d1ee38ce269eef7c676fbb7
Susheeltester <sushilamharsh7860@gmail.com>
2024-07-07 00:29:13 -0400

    New Class created for browser.

---
+++ 21MayProject/src/main/java/applicationUtility/BrowserLauncher.java
@@ -0,0 +1,11 @@
+package applicationUtility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserLauncher {
+	
+public static void main(String[] args) {
+	WebDriver driver = new ChromeDriver();
+}
+}
